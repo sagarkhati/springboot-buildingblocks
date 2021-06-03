842c74dc2c775d727af8ac4bbf6f6158d8e23090
sagarkhati <sonu06sagar@gmail.com>
2021-06-03 20:32:54 +0530

    i18n - First Commit

--- src/main/java/com/sagarkhati/restservices/Hello/HelloWorldController.java
+++ src/main/java/com/sagarkhati/restservices/Hello/HelloWorldController.java
@@ -1,22 +1,41 @@
 package com.sagarkhati.restservices.Hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
 public class HelloWorldController {
 
-	//Simple Method
-	//URI - /helloWorld
-	//GET
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
+
+	// Simple Method
+	// URI - /helloWorld
+	// GET
 	@GetMapping("/helloworld1")
 	public String hellowWorld() {
 		return "Hello World";
 	}
-	
+
 	@GetMapping("/helloworld-bean")
 	public UserDetails helloWorldBean() {
-		return new UserDetails("Sagar","Khati","Khatima");
+		return new UserDetails("Sagar", "Khati", "Khatima");
+	}
+
+	@GetMapping("/hello-int")
+	public String getMessagesInI18NFormat(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+		return messageSource.getMessage("label.hello", null, new Locale(locale));
+	}
+	
+	@GetMapping("/hello-int2")
+	public String getMessagesInI18NFormat() {
+		return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
 	}
 }
